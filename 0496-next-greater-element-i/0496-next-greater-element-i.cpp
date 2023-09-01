100efc94fee81b0b36db64fb97bbcd1474691279
Rohit Raj <88617637+rohit3716@users.noreply.github.com>
2023-09-01 17:01:20 +0530

    Time: 7 ms (52.26%), Space: 8.9 MB (33.23%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        unordered_map<int, int> umap;
+        int n = nums2.size();
+        for( int i = n-1; i >= 0; i-- ) {
+            int element = nums2[i];
+            while( !st.empty() && st.top() <= element) {
+                st.pop();
+            } 
+            int res = (st.empty()) ? -1 : st.top();
+            umap.insert({element, res});
+            st.push(element);           
+        }
+
+        vector<int> ans;
+        for( auto x:nums1 ) {
+            ans.push_back(umap[x]);
+        }
+        return ans;
+    }
+};
